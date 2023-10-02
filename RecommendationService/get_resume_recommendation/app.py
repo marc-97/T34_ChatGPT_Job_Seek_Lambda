19cb7b20ef621803fec57957c590b6814f61380d
KJ-TAM-QR4M3\deepadmin <kaijun.tam@ncs.com.sg>
2023-10-02 18:23:11 +0800

    fetch file from s3

--- RecommendationService/get_resume_recommendation/app.py
+++ RecommendationService/get_resume_recommendation/app.py
@@ -3,23 +3,24 @@ import boto3
 
 def lambda_handler(event, context):
 
-    resumeText = convert_s3_file_to_text(event['obectKey'])
-    print(resumeText)
+    # resume_text = convert_s3_file_to_text(event.objectKey)
+    
+
 
     return {
         "statusCode": 200,
         "body": json.dumps(
             {
-                "message": "hello world",
+                # "message": resume_text,
+                "message": event,
                 # "location": ip.text.replace("\n", "")
             }
         ),
     }
 
-def convert_s3_file_to_text(obect_key):
+def convert_s3_file_to_text(object_key):
     s3 = boto3.client('s3')
     bucket_name = 't34/documents'
-    object_key = 'your-file-name'
     response = s3.get_object(Bucket=bucket_name, Key=object_key)
     data = response['Body'].read().decode('utf-8')
     print(data)
