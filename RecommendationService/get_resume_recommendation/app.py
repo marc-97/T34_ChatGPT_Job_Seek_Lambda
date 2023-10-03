7d89bb298f5f176364b784443b3033ee273617e3
KJ-TAM-QR4M3\deepadmin <kaijun.tam@ncs.com.sg>
2023-10-03 10:58:41 +0800

    integrate with s3 and open ai

--- RecommendationService/get_resume_recommendation/app.py
+++ RecommendationService/get_resume_recommendation/app.py
@@ -1,26 +1,41 @@
 import json
 import boto3
+import openai
 
 def lambda_handler(event, context):
-
-    # resume_text = convert_s3_file_to_text(event.objectKey)
-    
-
+    resume_text = convert_s3_file_to_text(event.get('objectKey'))
+    question = "Provide the below resume content, pretend yourself as an experienced interviewer, review and give some suggestion to the content. \n" + resume_text
+    print("Question to Open AI:", question)
+    res = get_response_from_open_ai(question)
 
     return {
         "statusCode": 200,
         "body": json.dumps(
             {
-                # "message": resume_text,
-                "message": event,
-                # "location": ip.text.replace("\n", "")
+                "message": res,
             }
         ),
     }
 
 def convert_s3_file_to_text(object_key):
-    s3 = boto3.client('s3')
-    bucket_name = 't34/documents'
+    aws_session = boto3.Session(
+        aws_access_key_id='xxx',
+        aws_secret_access_key='xxx',
+    )
+    s3 = aws_session.client('s3')
+
+    bucket_name = 't34'
     response = s3.get_object(Bucket=bucket_name, Key=object_key)
     data = response['Body'].read().decode('utf-8')
-    print(data)
+    return data
+    
+def get_response_from_open_ai(question):
+    openai.api_key = 'xxx'
+
+    response = openai.Completion.create(
+        model = "text-davinci-003",
+        prompt = question,
+        temperature = 1,
+        max_tokens = 100)
+    
+    return response['choices'][0]['text']
